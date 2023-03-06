506f05e6a3fb90215306290da695881e99e6a6e8
Cauê Ghelfi <107440564+CaueGhelfi@users.noreply.github.com>
2023-03-06 14:21:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+
+const listaTeclas = document.querySelectorAll(".tecla");
+
+function tocaSom(seletorAudio){
+
+const elemento = document.querySelector(seletorAudio);
+
+if(elemento != null && elemento.localName === 'audio'){
+        elemento.play();
+}else{
+    alert("Elemente nao encontrado!");
+}
+
+}
+
+
+
+for(var i =0; i<listaTeclas.length;i++){
+   
+const tecla = listaTeclas[i];
+const instrumento = tecla.classList[1];
+const idAudio = `#som_${instrumento}`;
+
+
+
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    };
+
+    tecla.onkeydown = function(evento){
+        if(evento.code == 'Space' || evento.code == 'Enter')
+        tecla.classList.add('ativa');
+       
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+}
